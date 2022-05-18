33ec9580a8ef8d198d5e7c780bae11184b12b2aa
Utkarsh keshari <80323216+utkarshbanka@users.noreply.github.com>
2022-05-18 22:28:29 +0530

    Add files via upload

---
+++ firstapp/src/com/masai/Demo.java
@@ -0,0 +1,46 @@
+package com.masai;
+
+public class Demo {
+
+	
+	public static void main(String [] args) {
+	
+		System.out.println("Name : Utkarsh keshari");
+		System.out.println("Father’s Name : Mr vinod keshari");
+		System.out.println("Mother ‘s Name : Mrs Babita keshari");
+		System.out.println("Age : 18");
+		System.out.println("Gender : Male");
+		System.out.println("Address : Near Union bank siswa bazar maharajganj");
+		System.out.println("Mobile : 9999444349");
+	}
+}
+/*
+Write the difference between JRE, JDK and JVM?
+
+Answer = JRE is majorly responsible for creating environment for code execution.
+         and jdk is  used for code execution ;
+         JVM helps to change the intermidiate level english to system language thats system understand
+         what the they write;
+
+ How does java achieve platform independency?
+Answer = Java have platfrom  independency becouse when we write one application for windows thats application 
+also work in the mac and the linux and its only needs java jre that helps java code to translate in
+the mac or linux system format
+
+
+
+
+
+What are features of Java and explain them?
+answer = java is high level object orineted program and easy to learn language and 
+and java has platfrom independcy when we write code for one system they run on all the system 
+
+
+
+
+
+
+
+
+
+*/
